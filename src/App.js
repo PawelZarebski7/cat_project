d6a28f46484a56709e5b82ac618a63c763555a3a
Paweł <pawelzarebski7@wp.pl>
2022-10-23 23:09:53 +0200

    Add favoicon, clear code

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect, useRef} from 'react';
+import React, {useState, useEffect} from 'react';
 import "./App.css";
 import { TextField } from '@material-ui/core';
 import Radio from '@mui/material/Radio';
@@ -7,13 +7,9 @@ import FormControlLabel from '@mui/material/FormControlLabel';
 import FormControl from '@mui/material/FormControl';
 import FormLabel from '@mui/material/FormLabel';
 import Box from '@mui/material/Box';
-import useSound from 'use-sound';
 import { styled } from '@mui/material/styles';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
-
-
-// import mySound from './assets/Sound.mp3'
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -25,36 +21,13 @@ const Item = styled(Paper)(({ theme }) => ({
   height: '500px'
 }));
 
-const { CAT_KEY } = process.env;
-
 function App() {
 
   const [cat, setCat] = useState('');
   const [img, setImg] = useState('');
   const [radio, setRadio] = useState('');
   const [loading, setLoading] = useState(false);
-  const [errorStatus, setErrorStatus] = useState(true);
-
-  const imageUrl = process.env.REACT_APP_CAT_KEY + cat;
-  const waitTime = 1000;
-
-  console.log(process.env.REACT_APP_CAT_KEY);
-
-  const fetchImage = async () => {
-  
-  const res = await fetch(imageUrl);
-  console.log(res)
-    if (!res.status >= 200 && !res.status <= 299) {
-        const jsonResponse = await res.status;
-        console.log(jsonResponse);
-        console.log(res.status, res.statusText);
-        setErrorStatus([res.status, res.statusText])
-        } 
-    const imageBlob = await res.blob();
-    const imageObjectURL = URL.createObjectURL(imageBlob);
-    setImg(imageObjectURL);
-    setLoading(false);
-  }
+  const [errorStatus, setErrorStatus] = useState(null);
 
   const handleChange = e => {
     const target = e.target;
@@ -63,91 +36,96 @@ function App() {
     }
  };
   
-  const notInitialRender = useRef(false);
-
   useEffect(() => { 
-    if (notInitialRender.current) {
 
-    
+    const imageUrl =  process.env.REACT_APP_CAT_KEY + cat;
+    const waitTime = 1000;
+
+    const fetchImage = async () => {
+      const res = await fetch(imageUrl);
+        if (res.status < 200 || res.status >= 300) {
+            setErrorStatus(res.status)
+            } 
+        const imageBlob = await res.blob();
+        const imageObjectURL = URL.createObjectURL(imageBlob);
+        setImg(imageObjectURL);
+        setLoading(false);
+      }
+
     if(cat !== '') {
       setLoading(true);
       setImg(null);
       setErrorStatus(null);
     }
+   
     const catTimer = setTimeout(() => fetchImage(), waitTime);
-    return () => { clearInterval(catTimer);
-    } } 
-    else if(notInitialRender.current) {
-      notInitialRender.current = false
-    }
-    else {
-      notInitialRender.current = true
-    }
+    return () => { clearInterval(catTimer);      
+    } 
   },[cat, radio])
-
-  // const [playSound] = useSound(mySound);
-
-  // const catSound = () => {
-  //   playSound()
-  // }
-
-  // useEffect(() => {
-  //   catSound();
-  // }, [])  
-    
- 
-     
- 
+  
   return (
     <Box className='child'>
        <Grid container spacing={2}>
         <Grid item xs={6}>
           <Item>
-            
-        <FormControl noValidate autoComplete='off' className='margin: auto'>
-        <TextField 
-                placeholder='Add Cat Name'
-                type="text" 
-                id='cat' 
-                name='cat'
-                onChange={(e) => setCat(e.target.value)}
-          />
-      
-           
- 
-        
-      <FormLabel id="demo-radio-buttons-group-label">Choose color</FormLabel>
-      <RadioGroup
-        aria-labelledby="demo-radio-buttons-group-label"
-        name="radio-buttons-group"
-       
-      >
-        <FormControlLabel type="radio" value="red" control={<Radio />} checked={radio === "red"} label="Red"  onChange={handleChange} />
-        <FormControlLabel type="radio" value="green" control={<Radio />} checked={radio === "green"} label="Green"  onChange={handleChange} />
-        <FormControlLabel type="radio" value="blue" control={<Radio />} checked={radio === "blue"} label="Blue"  onChange={handleChange} />
-      </RadioGroup>
-    </FormControl>
-          </Item>
+            <FormControl noValidate autoComplete='off' className='margin: auto'>
+              <TextField 
+                  placeholder='Add Cat Name'
+                  type="text" 
+                  id='cat' 
+                  name='cat'
+                  onChange={(e) => setCat(e.target.value)}
+              />
+              <FormLabel id="demo-radio-buttons-group-label"></FormLabel>
+                <RadioGroup
+                  aria-labelledby="demo-radio-buttons-group-label"
+                  name="radio-buttons-group"
+                  >
+                  <FormControlLabel 
+                    type="radio" 
+                    value="red" 
+                    control={<Radio />} 
+                    checked={radio === "red"} 
+                    label="Red"  
+                    onChange={handleChange} 
+                    />
+                  <FormControlLabel 
+                    type="radio" 
+                    value="green" 
+                    control={<Radio />} 
+                    checked={radio === "green"} 
+                    label="Green"  
+                  onChange={handleChange} 
+                  />
+                  <FormControlLabel 
+                    type="radio" 
+                    value="blue" 
+                    control={<Radio />} 
+                    checked={radio === "blue"} 
+                    label="Blue"  
+                    onChange={handleChange} 
+                  />
+                </RadioGroup>
+              </FormControl>
+                {
+                  !cat && <h1 style={{marginTop: '2em'}}>Use form to generate cat</h1>
+                }
+            </Item>
         </Grid>
-        { cat && 
-        <Grid item xs={6}>
-          <Item><div>
-             {
-              loading && <h1>loading...</h1>
-             }
-             {
-              errorStatus && <h1>Cat could not be generated</h1>
-             }
-              <img src={img} alt="icons" className='margin: auto' />
-        </div></Item>
-        </Grid>
-        }
-      </Grid>
-        
-          
-       
-        
-       
+                { cat && 
+                  <Grid item xs={6}>
+                    <Item>
+                      {
+                        loading && <h1>loading...</h1>
+                      }
+                      {
+                        errorStatus && <h1>Cat could not be generated</h1>
+                      }
+                      <img src={img} alt="icons" className='margin: auto' />
+                    </Item>
+                  </Grid>
+                }
+          </Grid>
     </Box>
   );
 }
